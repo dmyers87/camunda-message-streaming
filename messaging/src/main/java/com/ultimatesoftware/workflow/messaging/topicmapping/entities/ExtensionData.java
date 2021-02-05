ccbd87d971b650c2916a54024844f04ba1e86a20
Leonardo De la Cruz <Leonardo.DelaCruz@ukg.com>
2021-02-05 09:28:55 -0500

    Change the strategy for generating id for each entry of the database.

--- messaging/src/main/java/com/ultimatesoftware/workflow/messaging/topicmapping/entities/ExtensionData.java
+++ messaging/src/main/java/com/ultimatesoftware/workflow/messaging/topicmapping/entities/ExtensionData.java
@@ -24,7 +24,7 @@ public class ExtensionData {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
